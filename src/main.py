2afa1f736e842a9999045a809f146d8f3585239c
SimonCastonguayETS <simon.castonguay.3@ens.etsmtl.ca>
2024-03-12 13:10:14 -0400

    pull

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ class App:
         self.TICKS = 10
 
         # To be configured by your team
-        self.HOST = None  # Setup your host heretetet
+        self.HOST = None  # Setup your host here
         self.TOKEN = None  # Setup your token here
         self.T_MAX = None  # Setup your max temperature here
         self.T_MIN = None  # Setup your min temperature here
